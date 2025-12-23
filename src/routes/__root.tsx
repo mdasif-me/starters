e53a31ea61201a71ac6b134ea80fdf7c6ada180e
mdasif-me <cn.mdasif@gmail.com>
2025-12-23 11:14:26 +0600

    feat: introduce root route with main layout, global search, and integrated devtools, alongside dependency updates.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -6,8 +6,6 @@ import {
 } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 
-import ClerkProvider from '../integrations/clerk/provider'
-
 import StoreDevtools from '../lib/demo-store-devtools'
 
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
@@ -36,60 +34,58 @@ interface MyRouterContext {
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
-      <ClerkProvider>
-        <SidebarProvider>
-          <AppSidebar />
-          <SidebarInset>
-            <header className="bg-background z-50 sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4 justify-between">
-              <div className="flex items-center gap-2">
-                <SidebarTrigger className="-ml-1 text-muted-foreground" />
-                <Separator
-                  orientation="vertical"
-                  className="mr-2 data-[orientation=vertical]:h-9"
-                />
-                <div className="lg:block md:hidden sm:block hidden">
-                  <GlobalSearch />
-                </div>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="bg-background z-50 sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4 justify-between">
+            <div className="flex items-center gap-2">
+              <SidebarTrigger className="-ml-1 text-muted-foreground" />
+              <Separator
+                orientation="vertical"
+                className="mr-2 data-[orientation=vertical]:h-9"
+              />
+              <div className="lg:block md:hidden sm:block hidden">
+                <GlobalSearch />
               </div>
+            </div>
+            <div className="flex items-center gap-3">
               <div className="flex items-center gap-3">
-                <div className="flex items-center gap-3">
-                  <Avatar className="size-9">
-                    <AvatarImage src={`${user}`} alt={`user profile`} />
-                    <AvatarFallback>AS</AvatarFallback>
-                  </Avatar>
-                  <div className="flex flex-col">
-                    <Link
-                      to="/"
-                      className="text-sm font-medium text-foreground hover:text-primary"
-                    >
-                      {'Kathryn Campbell'}
-                    </Link>
-                    <div className="text-xs font-normal text-muted-foreground">
-                      {'kathryn.campbell@example.com'}
-                    </div>
+                <Avatar className="size-9">
+                  <AvatarImage src={`${user}`} alt={`user profile`} />
+                  <AvatarFallback>AS</AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col">
+                  <Link
+                    to="/"
+                    className="text-sm font-medium text-foreground hover:text-primary"
+                  >
+                    {'Kathryn Campbell'}
+                  </Link>
+                  <div className="text-xs font-normal text-muted-foreground">
+                    {'kathryn.campbell@example.com'}
                   </div>
                 </div>
               </div>
-            </header>
-            <div className="flex flex-1 flex-col gap-4 p-4">
-              <Outlet />
             </div>
-          </SidebarInset>
-        </SidebarProvider>
-        <TanStackDevtools
-          config={{
-            position: 'bottom-right',
-          }}
-          plugins={[
-            {
-              name: 'Tanstack Router',
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-            StoreDevtools,
-            TanStackQueryDevtools,
-          ]}
-        />
-      </ClerkProvider>
+          </header>
+          <div className="flex flex-1 flex-col gap-4 p-4">
+            <Outlet />
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+          StoreDevtools,
+          TanStackQueryDevtools,
+        ]}
+      />
     </>
   ),
 })
