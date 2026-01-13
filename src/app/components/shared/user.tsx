2f1d5dc1e3c5872ae352b2865b0b6b1e02d4e991
mdasif-me <cn.mdasif@gmail.com>
2026-01-13 14:51:08 +0600

    feat: update User component to display dynamic user name and email; remove console log

--- src/app/components/shared/user.tsx
+++ src/app/components/shared/user.tsx
@@ -5,12 +5,16 @@ import user_icon from '../../../../public/img/user.svg';
 
 export default function User() {
   const { user } = useAuth();
-  console.log('user', user);
+
   return (
     <div className='flex items-center gap-3'>
       <article>
-        <h1 className='text-sm text-[#0F172B] text-end'>Admin User</h1>
-        <p className='text-[#62748E] text-xs text-end'>{user?.email}</p>
+        <h1 className='text-sm text-[#0F172B] text-end'>
+          {user?.name ?? 'ZMC User'}
+        </h1>
+        <p className='text-[#62748E] text-xs text-end'>
+          {user?.email ?? 'user@zmc.com'}
+        </p>
       </article>
       <Image src={user_icon} alt='User' />
     </div>
