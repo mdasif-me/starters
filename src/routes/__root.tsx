c0264d2dee080468d980b5555e628b3a4111fb85
mdasif-me <cn.mdasif@gmail.com>
2026-01-14 12:55:12 +0600

    refactor: Simplify type imports in __root.tsx for better readability

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,11 +5,11 @@ import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
 import { AnchoredToastProvider, ToastProvider } from '@/components/ui/toast'
+import type { ERole, IUser } from '@/features/auth/types'
 import type { QueryClient } from '@tanstack/react-query'
-import type { ERole } from '@/features/auth/types'
 
 interface AuthHelpers {
-  getUser: () => import('@/features/auth/types').IUser | null
+  getUser: () => IUser | null
   hasRole: (roles: ERole[]) => boolean
   hasPermission: (permission: string) => boolean
 }
