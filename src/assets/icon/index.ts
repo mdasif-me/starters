2b4e9d0963c95c4559a0aa52d4c026137985e3b4
mdasif-me <cn.mdasif@gmail.com>
2026-02-15 13:16:34 +0600

    feat: update layout and assets with new fonts and SVG images

---
+++ src/assets/icon/index.ts
@@ -0,0 +1,11 @@
+import chat from './chat.svg';
+import star from './star.svg';
+import support from './support.svg';
+
+const icons = {
+  support,
+  star,
+  chat,
+};
+
+export default icons;
---
+++ src/assets/img/index.ts
@@ -0,0 +1,9 @@
+import hero_app_screen from './hero/app-screen.svg';
+import hero_gradient from './hero/gradient.svg';
+
+const images = {
+  hero_app_screen,
+  hero_gradient,
+};
+
+export default images;
